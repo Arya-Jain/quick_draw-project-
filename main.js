11affddc430e209986862a8555e87a106e9f184e
Arya-Jain <89778433+Arya-Jain@users.noreply.github.com>
2021-12-22 20:40:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,23 +24,36 @@ function updateCanvas(){
 
 function setup(){
     canvas = createCanvas(280,280);
-    canvas.position(390,280);
+    canvas.center();
     background("white");
+
+    canvas.mouseReleased(classifyCanvas);
+
+    //additional activity which i am doing !!
+    synth = window.speechSynthesis;
 }
 
 function draw(){
     check_sketch();
+
     if(drawn_sketch == sketch){
         answer_holder = "set";
         score = score + 1;
         document.getElementById("score").innerHTML = "Score : " + score;
+    }
+
+    strokeWeight(10);
+     stroke("grey");
+
+     if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
     }
 }
 
 function check_sketch(){
     timer_counter++;
     document.getElementById("timer").innerHTML = "Timer : " + timer_counter;
-    console.log(timer_counter);
+    
 
     if(timer_counter>400){
         timer_counter = 0;
@@ -54,3 +67,32 @@ function check_sketch(){
     }
 }
 
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+
+        drawn_sketch = results[0].label;
+        document.getElementById("sketch").innerHTML = "Your Sketch : " + drawn_sketch ; 
+
+        document.getElementById("confidence").innerHTML = "Confidence : " + Math.round(results[0].confidence * 100) + "%";
+
+        utter_this =  new SpeechSynthesisUtterance(drawn_sketch);
+        synth.speak(utter_this);
+    }
+    
+}
+
+function clearCanvas(){
+    background("white");
+}
